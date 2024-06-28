49e1777772b86e97aa3dbad75d054e5c3636c1e7
Duduenri <eduess290799@gmail.com>
2024-06-28 11:50:39 -0300

    extends e super

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
+import { Mago } from "./modules/mago.js"
 
-const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
-const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
+const magoDudu = new Mago('Dudu', 8, 'fogo', 4, 3)
+const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
 
-const personagens = [personagemPedrinho, personagemJose]
+const personagens = [magoDudu, magaJulia]
 
 new PersonagemView(personagens).render()
-
-console.log(personagemJose.vida)
